85716e4f3ca59f4b355cbb27c1b997d5b5c771d8
ayuush19 <133370514+ayuush19@users.noreply.github.com>
2023-05-30 11:32:57 -0700

    student_marks.java

--- average.java
+++ average.java
@@ -1,23 +1,50 @@
-package first_java;
-
-public class account {
-
-	public static void main(String[] args) {
-		
-		acc_info ai = new acc_info();   
-		
-		ai.setName("krushna");
-		System.out.println(ai.getName());
-		
-		ai.setbalance(5000);
-		System.out.println(ai.getbalance());
-		
-		
-		System.out.println("your remaining balance is : "+ai.withdraw(4000));
-		System.out.println("Now your balance is : "+ai.deposite(4000));
-	    System.out.println(ai.withdraw(1500));
-		
-
+	package first_java;
+	
+	import java.util.Scanner;
+	
+	public class student_marks {
+	
+		public static void main(String[] args) {
+	
+				Scanner num= new Scanner (System.in);
+				{
+				System.out.println("enter mark1= ");
+				int mark1= num.nextInt();
+				
+				System.out.println("enter mark2= ");
+				int mark2= num.nextInt();
+				
+				System.out.println("enter mark3= ");
+				int mark3= num.nextInt();
+				
+				System.out.println("enter mark4= ");
+				int mark4= num.nextInt();
+				
+				int sum=mark1+mark2+mark3+mark4;
+				int avg=sum/4;
+				{
+						if(avg>90)
+						{
+							System.out.println("Grade A");
+						}
+						else if(avg<80 && avg>90)
+						{
+							System.out.println("Grade B");
+						}
+						else if(avg<70 && avg>80)
+						{
+							System.out.println("Grade C");
+						}
+						else if(avg<70)
+						{
+							System.out.println("Fail!!");
+						}
+				}
+				num.close();
+			}
+	
+	
+		}
+	
+	
 	}
-
-}
